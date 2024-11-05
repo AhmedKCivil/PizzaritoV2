689bbbf8ddc2adeb819fe7925752d5597866340a
AhmedKCivil <Ahmed.K+1@live.se>
2024-11-05 23:43:30 +0000

    Incorporating more Javascript into the project for UI improvement !

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,10 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//Function to update Cart count
+function updateCartCount() {
+    const cartItems = JSON.parse(sessionStorage.getItem("cartItems")) || [];
+    const cartCount = cartItems.length;
 
-// Write your JavaScript code.
+    //Update the badge display
+    document.getElementById("cart-count").textContent = cartCount;
+
+}
+
